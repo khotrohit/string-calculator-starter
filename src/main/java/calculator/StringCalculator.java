8afe62cdbd32dbb04943440b84cc20804e663edf
D2_49567_ROHIT_KHOT <khotrohit12@gmail.com>
2021-10-17 11:35:33 +0530

    modified add method to accept 2 or more elements

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,10 +3,27 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+    	//if string is empty it returns 0
+    	int sum=0;
        if(input.isEmpty())
     	    return 0;
-       else
-    	   return Integer.parseInt(input);
-    }
-
+       else {
+    	   if(input.length()==1)
+    	      return Integer.parseInt(input);   //if string length is 1 it returns first element converted to integer
+    	   else {
+    		   //these will check for 2 or 3 numbers
+    		   String[] nums = StringCalculator.split(input);
+    		  
+    		   for (String num : nums) {
+    	            sum += Integer.parseInt(num);
+    	        }
+    		   return sum;
+    	   }
+         }
+      }
+    private static String[] split(String str)
+	{
+		String[] nums = str.split(",");
+		return nums;
+	}
 }
